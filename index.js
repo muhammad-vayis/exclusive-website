44216c4f2dffc76646f417b58bf90d59c90ed841
Muhammad Vayis p <muhammadvayis.talrop@gmail.com>
2024-11-22 18:10:03 +0530

    styling completed

---
+++ index.js
@@ -0,0 +1,5 @@
+document.getElementById("viewProductsButton").onclick = function() {
+    // Redirect to the products page
+    window.location.href = 'index.html';
+    console.log("Button clicked: Redirecting to products page.");
+};
